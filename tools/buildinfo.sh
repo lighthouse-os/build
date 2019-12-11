69c64f2d34e035137157dc075ea0131f3af47875
Dyneteve <dyneteve@pixelexperience.org>
2019-12-11 19:13:34 +0200

    build: Use CUSTOM_BUILD for OTA prop
    Change-Id: I3e417ff98af75927a1f366616363b49d5c356339

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -55,6 +55,6 @@ if [ -n "$BUILD_THUMBPRINT" ] ; then
   echo "ro.build.thumbprint=$BUILD_THUMBPRINT"
 fi
 
-echo "ro.lighthouse.device=$LIGHTHOUSE_DEVICE"
+echo "ro.lighthouse.device=$LIGHTHOUSE_BUILD"
 
 echo "# end build properties"
