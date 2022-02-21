dcab7153281855fbb983bf7abd9f5d6d03a48c8c
CannedShroud <hypersync112@gmail.com>
2022-02-22 01:47:24 +0530

    build: Call correct Board config

--- core/config.mk
+++ core/config.mk
@@ -317,7 +317,7 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
 ifneq ($(LIGHTHOUSE_BUILD),)
-include vendor/lighthouse/config/BoardConfig.mk
+include vendor/lighthouse/config/BoardConfigLighthouse.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
